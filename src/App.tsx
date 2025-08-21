75d4ccd68acfa6e50a8b317332feaad9ff39ae9e
deosina <116112638+Kriss010119@users.noreply.github.com>
2025-08-22 02:30:26 +0300

    pdate sentry

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,23 @@ import { Loading } from './components/Loading/Loading.tsx';
 import CreateBookManual from './components/CreateBookManual/CreateBookManual.tsx';
 import BookPage from './pages/BookPage/BookPage.tsx';
 import { WebVitals } from './components/WebVitals';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://96e07435093683eb4ebc7b7c4ac2105e@o4509884596420608.ingest.us.sentry.io/4509884664446976',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 function App() {
   const { isAuth } = useAuth();
@@ -72,4 +89,4 @@ function App() {
   );
 }
 
-export default App;
+export default Sentry.wrap(App);
