7b8a78a0ec432e670c277b972d5275dca9071692
deosina <116112638+Kriss010119@users.noreply.github.com>
2025-08-22 02:17:01 +0300

    sentry added

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,24 @@ import { Provider } from 'react-redux';
 import { AuthProvider } from './context/auth-context';
 import App from './App';
 import { store } from './store/store.tsx';
-
 import './index.css';
+
+import * as Sentry from "@sentry/react";
+import { StrictMode } from "react";
+
+Sentry.init({
+  dsn: "https://a869374fa7b5e21771a6600f6876d838@o4509884596420608.ingest.us.sentry.io/4509884598779904",
+  sendDefaultPii: true
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <AuthProvider>
-          <App />
+          <StrictMode>
+            <App />
+          </StrictMode>
         </AuthProvider>
       </Provider>
     </BrowserRouter>
